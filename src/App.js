0511946e45e61a5ba731d29b24ef6554682eaece
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-01 12:04:00 +0100

    Removed unnecessary files and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Application</h1>
       </header>
+      <footer className="App-footer">
+        <p>This project was coded by Jessica Pintado and is open-sourced on GitHub.</p>
+      </footer>
     </div>
   );
 }
