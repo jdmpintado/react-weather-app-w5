ba7e34c9f3eaa9770eae660e248a9ecd7dee7252
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-01 12:15:29 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Application</h1>
-      </header>
-      <footer className="App-footer">
-        <p>
-          This project was coded by Jessica Pintado and is{" "}
-          <a
-            href="https://github.com/jdmpintado/react-weather-app-w5"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-          .
-        </p>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather Application</h1>
+        </header>
+        <footer className="App-footer">
+          <p>
+            This project was coded by Jessica Pintado and is{" "}
+            <a
+              href="https://github.com/jdmpintado/react-weather-app-w5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
