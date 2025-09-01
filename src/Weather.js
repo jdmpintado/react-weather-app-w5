d8a106a891fffbf572c3ecb9a9ea2819b7721b86
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-01 12:56:26 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -12,10 +12,15 @@ export default function Weather(props) {
               placeholder="Enter city..."
               className="form-control"
               required
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,13 +29,18 @@ export default function Weather(props) {
         <li>Wednesday 7:00</li>
         <li>Condition: Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/mostly_sunny_light.svg"
-            alt="Temperature Icon"
-          />
-          20°C
+          <div className="clearfix">
+            <img
+              src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/mostly_sunny_light.svg"
+              alt="Temperature Icon"
+              className="float-left"
+            />
+            <span className="temperature">
+              20<span className="unit">°C</span>
+            </span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
