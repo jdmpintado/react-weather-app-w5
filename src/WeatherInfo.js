96c33b64b287b3733ee1ca4b234a8b8a13163117
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-01 17:22:58 +0100

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -8,28 +9,34 @@ export default function WeatherInfo(props) {
       <h1>
         {props.data.city}, {props.data.country}
       </h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
+      <p>
+        <FormattedDate date={props.data.date} />
+      </p>
+      <div className="row mt-2">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src={props.data.iconURL}
-              alt="{props.data.icon}"
-              className="float-left"
-            />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-              <span className="unit">°C</span>
-            </span>
-          </div>
+            <img src={props.data.iconURL} alt="{props.data.icon}" className="float-left" />
+            <WeatherTemperature celsius={props.data.temperature} className="float-left" />
+      <p>
+        <FormattedDate date={props.data.date} />
+      </p>
+      <div className="row mt-2">
+        <div className="col-7">
+          <img
+            src={props.data.iconURL}
+            alt="{props.data.icon}"
+            className="float-left"
+          />
+          <WeatherTemperature
+            celsius={props.data.temperature}
+          />
         </div>
         <div className="col-5">
           <ul>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>
+              Humidity:
+              {props.data.humidity}%
+            </li>
             <li className="text-capitalize">{props.data.description}</li>
             <li>
               Humidity:
